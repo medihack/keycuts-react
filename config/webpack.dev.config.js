4761eaa07974093ca0a77babccbe42daf1e36fec
Kai Schlamp <kai.schlamp@gmail.com>
2018-06-04 12:41:49 +0000

    Switch to Webpack include rules (cause of problems with npm link).

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -8,10 +8,6 @@ const baseConfig = require('./webpack.base.config.js')
 const devConfig = {
   mode: 'development',
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: path.resolve(__dirname, '../docs/demo'),
-    publicPath: '/dist/'
-  },
   output: {
     filename: '[name].js'
   }
